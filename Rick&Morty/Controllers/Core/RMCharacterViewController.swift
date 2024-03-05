a269f74f75e8b012174d65f2a6808f2a6a385037
Artem Elchev <elarevs@Artems-Laptop.local>
2024-03-05 13:49:56 +0200

    Added character cell

--- Rick&Morty/Controllers/Core/RMCharacterViewController.swift
+++ Rick&Morty/Controllers/Core/RMCharacterViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 /// Controller for show and search for Characters
 final class RMCharacterViewController: UIViewController {
     
-    private let characterListView = CharacterListView()
+    private let characterListView = RMCharacterListView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
